a28ba9322e8308bdab1cdb1dff521f11cf94d0d4
obajide028 <obajide028@gmail.com>
2024-01-16 16:10:56 +0100

    asyncHandler middleware added and slugify and geocoder added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,57 +1,42 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 //@desc     Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = async (req, res, next) => {
-    try {
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
         const bootcamps = await Bootcamp.find();
         res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps});
-    } catch (error) {
-       next(error);
-    }
-   
-}
+});
 
 //@desc     Get single bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = async (req, res, next) => {
-    try {
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id)
         if(!Bootcamp){
            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
-        res.status(200).json({ success: true, msg: bootcamp });
-    } catch (error) {
-       next(error);
-    }
-  
-}
+        res.status(200).json({ success: true, msg: bootcamp }); 
+});
 
 //@desc     Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = async (req, res, next) => {
-    try {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.create(req.body);
 
         res.status(201).json({
             success: true,
             data: bootcamp
         });
-    } catch (error) {
-       next(error);
-    }
-  
-}
+});
 
 //@desc     Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = async (req, res, next) => {
-    try {
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -61,24 +46,15 @@ exports.updateBootcamp = async (req, res, next) => {
         }
     
         res.status(200).json({ success: true, data: bootcamp});
-    } catch (error) {
-        next(error);
-    }
- 
-}
+});
 
 //@desc     Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = async (req, res, next) => {
-    try {
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
         if(!bootcamp){
             return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
-    
         res.status(200).json({ success: true, data: {}});
-    } catch (error) {
-        next(error);
-    }
-}
+});
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geoCoder');
 
 const BootCampSchema = mongoose.Schema({
     name: {
@@ -98,15 +100,30 @@ const BootCampSchema = mongoose.Schema({
     }
 });    
 
-// // create bootcamp slug from the name
-// BootCampSchema.pre('save', function(next){
-//     this.slug = slugify(this.name, {lower: true});
-//     next();
-// });
+// create bootcamp slug from the name
+BootCampSchema.pre('save', function(next){
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
 
-// // Geocode & create location field
-// BootCampSchema.pre('save', async function(next){
-//     const loc = await 
-// })
+//Geocode & create location field
+BootCampSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode
+    }
+    //Do not save address in DB
+    this.address = undefined;
+    
+    next();
+})
+
 
 module.exports = mongoose.model('Bootcamp', BootCampSchema);
