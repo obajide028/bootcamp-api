446337ca589b97f0107db469c1e5977b0c7ecce7
obajide028 <obajide028@gmail.com>
2024-03-04 14:08:45 +0100

    forgot password, reset password and email added

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -37,6 +38,9 @@ const UserSchema = new mongoose.Schema({
 
   // Encrypt password using bcrypt
   UserSchema.pre('save', async function(next){
+     if(!this.isModified('password')){
+        next();
+     }
      const salt = await bcrypt.genSalt(10);
      this.password = await bcrypt.hash(this.password, salt);
   });
@@ -52,5 +56,25 @@ const UserSchema = new mongoose.Schema({
 UserSchema.methods.matchPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
 }
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    try {
+        // Hash token and set to resetPasswordToken field
+        this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+        // Set expire 
+        this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+        return resetToken;
+    } catch (error) {
+        // Handle error
+        console.error('Error generating reset password token:', error);
+        throw new Error('Failed to generate reset password token');
+    }
+};
 
 module.exports = mongoose.model('User', UserSchema);
